0c87ecda147ed79f2989a45f0f87752ae486232a
Aisha Hasanguliyeva <ahasanguliyeva02@gmail.com>
2025-02-10 10:45:11 +0400

    Initial Commit

---
+++ RunMap/RunMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunMapApp.swift
+//  RunMap
+//
+//  Created by Aisha on 10.02.25.
+//
+
+import SwiftUI
+
+@main
+struct RunMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
